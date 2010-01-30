fef2367633ec6a13c764566337bbe96dd6d7c1ac
rubiii <me@rubiii.com>
2010-01-31 00:00:12 +0100

    fixed deprecation warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require "spec"
 require "rspec_validation_expectations"
 
-require "activesupport"
-require "activerecord"
+require "active_support"
+require "active_record"
 require "validation_reflection"
 
 require "test_class"
